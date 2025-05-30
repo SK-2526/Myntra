1a28871b15cbb5c19a0c680b9400e2bcc9a3b766
Sadique <sadiquejet.sk2526@gmail.com>
2025-05-30 16:31:30 +0530

    updated index.js

--- src/components/data/store/index.js
+++ src/components/data/store/index.js
@@ -3,11 +3,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import itemSlice from "./itemSlice";
 import fetchStatusSlice from "./fetchStatusSlice";
 import bagSlice from './bagSlice';
+import mensSlice from "./mensSlice";
+import womenSlice from "./womenSlice";
+import KidSlice from "./KidSlice";
+import HomeSlice from "./HomeSlice";
+import BeautySlice from "./BeautySlice";
+
 
 const myntraStore= configureStore({
     reducer: {
     items: itemSlice.reducer,
     fetchStatus: fetchStatusSlice.reducer,
+    mens: mensSlice.reducer,
+    womens:womenSlice.reducer,
+    kids: KidSlice.reducer,
+    home: HomeSlice.reducer,
+    beauty: BeautySlice.reducer,
     bag: bagSlice.reducer,
     }
 });
