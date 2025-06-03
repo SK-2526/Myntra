5b779feb90aa9077042d7d109ab4260606079a49
Sadique <sadiquejet.sk2526@gmail.com>
2025-06-03 23:02:11 +0530

    updated

--- src/components/data/store/routes/Beauty.jsx
+++ src/components/data/store/routes/Beauty.jsx
@@ -1,17 +1,39 @@
-import BeautyItem from '../components/BeautyItem'
+import BeautyItems from '../components/BeautyItem'
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import Filter from './Filter';
 
 const Beauty = () => {
   
   const items= useSelector((store)=> store.beauty);
-  
+  const [filters, setFilters] = useState({ searchTerm: '', sortOrder: '' });
+       
+       const filteredItems = items
+       .filter((item) =>
+         `${item.item_name} ${item.company}`
+           .toLowerCase()
+           .includes(filters.searchTerm.toLowerCase())
+       )
+       .sort((a, b) => {
+         if (filters.sortOrder === 'HightoLow') {  // Descending (High to Low)
+           return b.current_price - a.current_price;
+         } else if (filters.sortOrder === 'LowtoHigh') {  //Ascending (Low to High)
+           return a.current_price - b.current_price;
+         }
+         return 0;
+       });
+
   return (
-    <main>
-    <div className="items-container">
-    {items.map((item =>
-         <BeautyItem key={item.id} item={item} />) )}
+    <div className="products">
+      <main className="main">
+        <Filter onFilterChange={setFilters} />
+        <div className="items-container">
+          {filteredItems.map((item) => (
+            <BeautyItems key={item.id} item={item} />
+          ))}
+        </div>
+      </main>
     </div>
-  </main>
   )
 }
 
